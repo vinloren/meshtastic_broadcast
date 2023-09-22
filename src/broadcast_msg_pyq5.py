2c0c67a175cc9af4506124566b5067ccccb06582
Vincenzo Lorenzale <vinloren@gmail.com>
2023-09-22 21:04:24 +0200

    Aggiunto data in messaggio di duplicazione

--- src/broadcast_msg_pyq5.py
+++ src/broadcast_msg_pyq5.py
@@ -748,7 +748,7 @@ class App(QWidget):
                     values('"+data+"','"+ora+"','"+str(msgID)+"','"+str(orig)+"','"+longname+"','"+tipmsg+"')"
                 self.insertDB(qr)
             else: 
-                msg = str(msgID)+" da "+str(orig)+" duplicato"
+                msg = datetime.datetime.now().strftime("%d/%m/%y %T")+" "+str(msgID)+" da "+str(orig)+" duplicato"
                 self.log.append(msg)    
 
         else:
